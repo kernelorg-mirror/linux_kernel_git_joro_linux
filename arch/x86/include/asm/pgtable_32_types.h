e0fa0f5cead26c28283ed46c7cd084700f46a9a5
Joerg Roedel <jroedel@suse.de>
2018-01-16 12:40:07 +0100

    x86/mm/ldt: Reserve high address-space range for the LDT
    Reserve 2MB/4MB of address space for mapping the LDT to
    user-space.
    
    Signed-off-by: Joerg Roedel <jroedel@suse.de>

--- arch/x86/include/asm/pgtable_32_types.h
+++ arch/x86/include/asm/pgtable_32_types.h
@@ -47,8 +47,11 @@ extern bool __vmalloc_start_set; /* set once high_memory is set */
 #define CPU_ENTRY_AREA_BASE				\
 	((FIXADDR_START - PAGE_SIZE * (CPU_ENTRY_AREA_PAGES + 1)) & PMD_MASK)
 
-#define PKMAP_BASE		\
+#define LDT_BASE_ADDR		\
 	((CPU_ENTRY_AREA_BASE - PAGE_SIZE) & PMD_MASK)
+
+#define PKMAP_BASE		\
+	((LDT_BASE_ADDR - PAGE_SIZE) & PMD_MASK)
 
 #ifdef CONFIG_HIGHMEM
 # define VMALLOC_END	(PKMAP_BASE - 2 * PAGE_SIZE)
