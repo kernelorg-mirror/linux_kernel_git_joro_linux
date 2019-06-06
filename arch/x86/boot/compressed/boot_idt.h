f54d6e67d2c78c048a7bf4881245a50d4ec7adf0
Joerg Roedel <jroedel@suse.de>
2019-06-06 12:03:55 +0200

    x86/boot/compressed/64: Install 32bit boot IDT
    The IDT will be used to install an early #VC exception
    handler.
    
    Signed-off-by: Joerg Roedel <jroedel@suse.de>

---
+++ arch/x86/boot/compressed/boot_idt.h
@@ -0,0 +1,46 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef __BOOT_IDT_H
+#define __BOOT_IDT_H
+
+#ifdef __ASSEMBLY__
+
+.macro BOOT32_IDT_ENTRY vector:req handler:req
+	push	%eax
+	push	%ebx
+	push	%ecx
+	push	%edx
+
+	/* Handler address in %eax */
+	leal	\handler(%ebp), %eax
+
+	/* IDT entry address to %ebx */
+	leal	boot32_idt(%ebp), %ebx
+	addl	$(8*\vector), %ebx
+
+	/* Build IDT entry, lower 4 bytes */
+	movl	%eax, %edx
+	andl	$0x0000ffff, %edx
+	movl	$__KERNEL32_CS, %ecx
+	shl	$16, %ecx
+	orl	%ecx, %edx
+
+	/* Store lower 4 bytes to IDT */
+	movl	%edx, (%ebx)
+
+	/* Build IDT entry, upper 4 bytes */
+	movl	%eax, %edx
+	andl	$0xffff0000, %edx
+	orl	$0x00008e00, %edx
+
+	/* Store upper 4 bytes to IDT */
+	movl	%edx, 4(%ebx)
+
+	pop	%edx
+	pop	%ecx
+	pop	%ebx
+	pop	%eax
+.endm
+
+#endif
+
+#endif
