d85ea56a52e51deb79e3b2988c8e152716964f41
Joerg Roedel <jroedel@suse.de>
2019-06-12 15:32:39 +0200

    x86/boot/compressed/64: Add GHCB MSR accessor functions
    The GHCB MSR is a central communication point of the SEV-ES
    guest to the hypervisor.
    
    Signed-off-by: Joerg Roedel <jroedel@suse.de>

--- arch/x86/boot/compressed/sev-es.c
+++ arch/x86/boot/compressed/sev-es.c
@@ -9,6 +9,27 @@
 #include <asm/svm.h>
 #include "misc.h"
 
+static inline u64 read_ghcb_msr(void)
+{
+	unsigned long low, high;
+
+	asm volatile("rdmsr\n" : "=a" (low), "=d" (high) :
+			"c" (MSR_AMD64_SEV_ES_GHCB));
+
+	return ((high << 32) | low);
+}
+
+static inline void write_ghcb_msr(u64 val)
+{
+	u32 low, high;
+
+	low  = val & 0xffffffffUL;
+	high = val >> 32;
+
+	asm volatile("wrmsr\n" : : "c" (MSR_AMD64_SEV_ES_GHCB),
+			"a"(low), "d" (high) : "memory");
+}
+
 void vc_handler(struct pt_regs *regs)
 {
 	/* Hang the machine for now */
