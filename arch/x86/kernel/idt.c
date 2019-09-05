980874b576a60bbe3a2999d8af77af58e05a04eb
Joerg Roedel <jroedel@suse.de>
2019-09-05 17:04:17 +0200

    x86/idt: Move IDT to data segment
    With SEV-ES, exception handling is needed very early, even before the
    kernel has cleared the bss segment. In order to prevent clearing the
    currently used IDT, move the IDT to the data segment.
    
    Signed-off-by: Joerg Roedel <jroedel@suse.de>

--- arch/x86/kernel/idt.c
+++ arch/x86/kernel/idt.c
@@ -166,7 +166,7 @@ static const __initconst struct idt_data dbg_idts[] = {
 #endif
 
 /* Must be page-aligned because the real IDT is used in a fixmap. */
-gate_desc idt_table[IDT_ENTRIES] __page_aligned_bss;
+gate_desc idt_table[IDT_ENTRIES] __page_aligned_data;
 
 struct desc_ptr idt_descr __ro_after_init = {
 	.size		= (IDT_ENTRIES * 2 * sizeof(unsigned long)) - 1,
