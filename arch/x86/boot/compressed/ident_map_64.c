dbb8895b638b4ed09753e3bf4108410a6fe25118
Joerg Roedel <jroedel@suse.de>
2019-12-05 11:39:56 +0100

    x86/boot/compressed/64: Don't pre-map memory in KASLR code
    With the page-fault handler in place we can build the identity mapping
    on-demand. There is no need to establish the mappings by hand, so
    remove the code and unexport/remove the functions to do so.
    
    Signed-off-by: Joerg Roedel <jroedel@suse.de>

--- arch/x86/boot/compressed/ident_map_64.c
+++ arch/x86/boot/compressed/ident_map_64.c
@@ -84,7 +84,7 @@ static struct x86_mapping_info mapping_info;
  * Once all ranges have been added, the new mapping is activated by calling
  * finalize_identity_maps() below.
  */
-void add_identity_map(unsigned long start, unsigned long size)
+static void add_identity_map(unsigned long start, unsigned long size)
 {
 	unsigned long end = start + size;
 
@@ -153,16 +153,6 @@ void initialize_identity_maps(void)
 	start = (unsigned long)_head;
 	size  = _end - _head;
 	add_identity_map(start, size);
-	write_cr3(top_level_pgt);
-}
-
-/*
- * This switches the page tables to the new level4 that has been built
- * via calls to add_identity_map() above. If booted via startup_32(),
- * this is effectively a no-op.
- */
-void finalize_identity_maps(void)
-{
 	write_cr3(top_level_pgt);
 }
 
