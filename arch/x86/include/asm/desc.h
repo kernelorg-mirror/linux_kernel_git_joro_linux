65c7dfafa881d3da36c86066f68b420cd6d1d101
Joerg Roedel <jroedel@suse.de>
2019-12-09 16:48:03 +0100

    x86/encrypted-state: Setup early #VC handler
    Setup an early handler for #VC exceptions. There is no GHCB mapped yet,
    so just re-use the no_ghcb_vc_handler. It can only handle CPUID
    exit-codes, but that should be enough to get the kernel through
    verify_cpu() and __startup_64() until it runs on virtual addresses.
    
    Signed-off-by: Joerg Roedel <jroedel@suse.de>

--- arch/x86/include/asm/desc.h
+++ arch/x86/include/asm/desc.h
@@ -388,6 +388,7 @@ static inline void set_desc_limit(struct desc_struct *desc, unsigned long limit)
 
 void update_intr_gate(unsigned int n, const void *addr);
 void alloc_intr_gate(unsigned int n, const void *addr);
+void set_early_idt_handler(gate_desc *idt, int n, void *handler);
 
 extern unsigned long system_vectors[];
 
--- arch/x86/include/asm/encrypted-state.h
+++ arch/x86/include/asm/encrypted-state.h
@@ -76,4 +76,6 @@ static inline u64 copy_lower_bits(u64 out, u64 in, unsigned bits)
 	return out;
 }
 
+extern void early_vc_handler(void);
+
 #endif
